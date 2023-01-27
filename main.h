3e43dd0097937a238507972a617ce740e905c544
Cara0608 <caraehizele0607@gmail.com>
2023-01-27 20:44:10 +0100

    Group project

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unsitd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/** STRUCTURES **/
+typedef struct print {
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} print_t;
+
+int _printf(const char *format, ...);
+int (*get_fine(char *s, int index))(va_list,char *, unsigned int);
+int handl_buf(char *buf, char *c, unsigned int ibuf);
+int print_buf(char *buf, unsigned int nbuf);
+
+int print_chr(va_list argument, char *buf, unsigned int ibuf);
+
+printf("%s I am %d years old.\n", "Hello Cara", 0, 4);
+
+#endif
